e048b3f176b1ce591861ca313d1f49c8bbcc8239
AdminY <orazgaliev99@mail.ru>
2022-06-21 13:10:24 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'generated/l10n.dart';
+import 'ui/persons_list/screen_persons_list.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.grey[50],
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ),
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: const Locale('ru', 'RU'),
+      supportedLocales: S.delegate.supportedLocales,
+      home: const PersonsList(),
+    );
+  }
+}
---
+++ lib/ui/login_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+import '../generated/l10n.dart';
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  String? login;
+  String? password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(S.of(context).auth),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        S.of(context).inputLoginAndPassword,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          label: Text(S.of(context).login),
+                        ),
+                        maxLength: 8,
+                        validator: (value) {
+                          if (value == null) return S.of(context).inputErrorCheckLogin;
+                          if (value.length < 3) {
+                            return S.of(context).inputErrorLoginIsShort;
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          login = value;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          label: Text(S.of(context).password),
+                        ),
+                        obscureText: true,
+                        maxLength: 16,
+                        validator: (value) {
+                          if (value == null) return S.of(context).inputErrorCheckPassword;
+                          if (value.length < 8) {
+                            return S.of(context).inputErrorPasswordIsShort;
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          password = value;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    child: Text(S.of(context).signIn),
+                    onPressed: () {
+                      final isValidated = formKey.currentState?.validate() ?? false;
+                      if (isValidated) {
+                        FocusScope.of(context).unfocus();
+                        formKey.currentState?.save();
+                        if (login == 'qwerty' && password == '123456ab') {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomeScreen(),
+                            ),
+                          );
+                        } else {
+                          Container();
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text(S.of(context).tryAgain),
+                              actions: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text(S.of(context).close),
+                                )
+                              ],
+                            ),
+                          );
+                        }
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
